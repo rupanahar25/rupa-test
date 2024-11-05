1e4e99aaeab2656ee9fa5a42fec94410fca83276
Rupa <rupa@Luthfuns-MacBook-Pro.local>
2024-11-05 14:46:13 +0600

    Bug Fixing

--- src/luthfunnahar.java
+++ src/luthfunnahar.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class luthfunnahar {
 
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         System.out.println("Hello World from Rupa");
 
         Scanner scanner= new Scanner(System.in);
         System.out.println("This is me: Luthfun Nahar");
         String message = scanner.nextLine();
-        System.out.println ("Again Hello");
+        System.out.println ("The next line is "+message);
+
         scanner.close();
     }
 }
