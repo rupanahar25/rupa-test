473c31a610a6de3807a55b8fce77e165386257ee
Rupa <rupa@Luthfuns-MacBook-Pro.local>
2024-11-05 12:44:30 +0600

    Add input parameter to HelloIntelliJ program

--- src/luthfunnahar.java
+++ src/luthfunnahar.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class luthfunnahar {
 
     public static void main(String args[])
     {
         System.out.println("Hello World from Rupa");
+
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("This is me: Luthfun Nahar");
+        String message = scanner.nextLine();
     }
 }
